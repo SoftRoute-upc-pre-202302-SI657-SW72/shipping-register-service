05fccdc9c7db9ea273eecd1a88f6b6a575badb09
Abel Cierto <u20201b561@upc.edu.pe>
2023-09-24 16:17:28 -0500

    Initial Commit

---
+++ src/main/java/upc/edu/pe/softroute/shippingregisterservice/ShippingRegisterServiceApplication.java
@@ -0,0 +1,13 @@
+package upc.edu.pe.softroute.shippingregisterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingRegisterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShippingRegisterServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/upc/edu/pe/softroute/shippingregisterservice/ShippingRegisterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package upc.edu.pe.softroute.shippingregisterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingRegisterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
