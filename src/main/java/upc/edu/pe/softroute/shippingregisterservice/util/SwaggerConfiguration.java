6f623b82e1d2313572bb7729cbb39218841131e9
Abel Cierto <u20201b561@upc.edu.pe>
2023-09-25 12:37:27 -0500

    feat: Swagger configuration

--- src/main/java/upc/edu/pe/softroute/shippingregisterservice/util/SwaggerConfiguration.java
+++ src/main/java/upc/edu/pe/softroute/shippingregisterservice/util/SwaggerConfiguration.java
@@ -1,4 +1,4 @@
-package util;
+package upc.edu.pe.softroute.shippingregisterservice.util;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
@@ -6,6 +6,9 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
 public class SwaggerConfiguration {
     public Docket Documentation() {
         return new Docket(DocumentationType.SWAGGER_2).select()
